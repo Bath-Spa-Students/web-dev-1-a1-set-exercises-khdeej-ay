35974c846a9f990e4a7e33d5a09b0ff6de443da7
Khadeeja <s.khdeej@gmail.com>
2024-04-04 23:12:05 +0400

    Add Chp 5 - Soundboard (js)

---
+++ Chapter-5-Embedded Content/Exercises/Alan Partridge Soundboard/index.js
@@ -0,0 +1,55 @@
+/* EXTERNAL JAVASCRIPT FILE */
+
+// Function that plays the "Ahha" audio
+function aha() {
+    var audio = document.getElementById("ah-ha");
+    audio.play();
+}
+
+// Function that plays the "Back Of The Net" audio
+function backOfTheNet() {
+    var audio = document.getElementById("back-of-the-net"); 
+    audio.play();
+}
+
+// Function that plays the "Bang Out Of Order" audio
+function bangOutOfOrder() {
+    var audio = document.getElementById("bang-out-of-order");
+    audio.play();
+}
+
+// Function that plays the "Beep" audio | self-insert
+function beep() {
+    var audio = document.getElementById("beep");
+    audio.play();
+}
+
+// Function that plays the "Dan" audio
+function dan() {
+    var audio = document.getElementById("dan");
+    audio.play();
+}
+
+// Function that plays the "Email Of The Evening" audio
+function emailOfTheEvening() {
+    var audio = document.getElementById("email-of-the-evening"); 
+    audio.play(); 
+}
+
+// Function that plays the "Hello Patridge" audio
+function helloPatridge() {
+    var audio = document.getElementById("hello-patridge");
+    audio.play();
+}
+
+// Function that plays the "I Ate A Scotch Egg" audio
+function iAteAScotchEgg() {
+    var audio = document.getElementById("i-ate-a-scotch-egg");
+    audio.play();
+}
+
+// Function that plays the "I'm Confused" audio
+function imConfused() {
+    var audio = document.getElementById("im-confused");
+    audio.play();
+}
